b4a584760a384b70f2d2b1460dcd88e21a0bc1d2
unknown <nadafilsa@gmail.com>
2023-05-09 15:09:16 +0700

    first commit

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,83 @@
+"use client";
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import { useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../input/Input";
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios
+      .post("./api/register", data)
+      .then(() => registerModal.onClose())
+      .catch((error) => toast.error(`${error}`))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Airbnb" subtitle="Create an account" />
+      <Input
+        id="email"
+        label="Email"
+        register={register}
+        disabled={isLoading}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        register={register}
+        disabled={isLoading}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        register={register}
+        disabled={isLoading}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+    />
+  );
+};
+
+export default RegisterModal;
